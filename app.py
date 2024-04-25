ee43ce92439973396351ec116696ae192ce6adbe
JordonLiMPW <157690407+JordonLiMPW@users.noreply.github.com>
2024-04-25 11:12:25 +0000

    wit password

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def home():
     password = request.args.get('password')
     if user == None:
         return render_template("index.html")
-    elif user == 'bob':
+    elif user == 'bob' and password == "114514":
         return "Hello " + user
     else:
         return "user not recognised"
