461766e85415d3d864db9b01e477166c0a0707a0
JordonLiMPW <157690407+JordonLiMPW@users.noreply.github.com>
2024-04-29 08:41:47 +0000

    save in text

--- app.py
+++ app.py
@@ -6,13 +6,25 @@ def home():
     if request.method == "GET":
         return render_template("index.html")
     else:
-        user = request.form['un']
-        password = request.form['pwd']
-        if user == 'bob' and password == "114514":
-            return "Hello " + user
+        f = open("login.txt", "r")
+        un = f.read()
+        pwd = f.read()
+        f.close()
+        print(un)
+        print(len(un))
+        if un[:-1] == request.form and pwd == request.form["pw"]:
+            return "Hello " + un
         else:
             return "user not recognised"
         
-@app.route("/signup")
+@app.route("/signup", methods = ["GET","POST"])
 def signup():
-    return render_template("signup.html")
+    if request.method == "GET":
+        return render_template("signup.html")
+    else:
+        f = open("login.txt", "w")
+        f.write(request.form['un'])
+        f.write("\n")
+        f.write(request.form['pwd'])
+        f.close()
+        return "sign up successful"
